4c1726b548632dc175a7b3baf0d15d201f5b5891
Danielprins1 <145456478+Danielprins1@users.noreply.github.com>
2024-11-06 01:10:39 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+var HamburgerMenu = document.querySelector('header > button');
+var nav = document.querySelector('nav');
+
+function MenuOpenen() {
+    nav.classList.toggle('HamburgerMenuOpenen'); 
+
+HamburgerMenu.addEventListener('click', MenuOpenen);
+
+document.addEventListener('click', function(event) {
+    if (!nav.contains(event.target) && !HamburgerMenu.contains(event.target)) {
+        nav.classList.remove('HamburgerMenuOpenen');
+    }
+});

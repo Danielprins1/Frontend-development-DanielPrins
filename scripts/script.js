9c73f892afadfbce6dc365b10f19a49af6f8e1e5
Danielprins1 <145456478+Danielprins1@users.noreply.github.com>
2024-11-06 09:54:31 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 // JavaScript Document
 console.log("hi");
 
+//CodeMentor. (2023, 25 november). How to create responsive navigation bar with HTML CSS And JavaScript [Video]. YouTube. https://www.youtube.com/watch?v=M498DvQDkJo
 var HamburgerMenu = document.querySelector('header > button');
 var nav = document.querySelector('nav');
 
